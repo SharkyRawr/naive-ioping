3458bc17588c911d0ff632c9c5b783d68e9cee79
Sophie Sharky Schumann <sharky@sharky.pw>
2017-11-08 02:26:49 +0100

    working O_DIRECT implementation? (Linux only)

--- ioping.c
+++ ioping.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/types.h>
 #include <time.h>
+#include <errno.h>
+#include <string.h>
 
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 #include "random_bytes.h"
 
@@ -14,16 +17,23 @@ int main(int argc, char** argv) {
 
 	clock_gettime(CLOCK_MONOTONIC, &tstart);
 
-	FILE* f = fopen("test.ping", "wb");
-	if(f == NULL) {
+	int fd = open("test.ping", O_CREAT|O_WRONLY|O_DIRECT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if(fd == -1) {
 		printf("Unable to open test.ping file!\n");
+		printf("Errno: %d %s\n", errno, strerror(errno));
 		return 1;
 	}
 
-	fwrite(___rand_bin, ___rand_bin_len, 1, f);
-	fclose(f);
+	ssize_t byteswritten = write(fd, ___rand_bin, ___rand_bin_len);
+	if(byteswritten == -1) {
+		unlink("test.ping");
+		printf("Unable to write bytes!\nerrno: %d %s\n", errno, strerror(errno));
+		return 1;
+	}
+	close(fd);
 	clock_gettime(CLOCK_MONOTONIC, &tend);
 	unlink("test.ping");
+	printf("wrote: %ld\n", byteswritten);
 
 	double seconds = tend.tv_sec - tstart.tv_sec;
 	double nanos = tend.tv_nsec - tstart.tv_nsec;
