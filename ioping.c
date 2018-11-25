71988b670680c99d4b4077feddb046d2a00fb205
Sophie Sharky Schumann <sharky@sharky.pw>
2018-11-25 15:23:13 +0100

    (breaking) Changed munin unit to seconds to allow for munins native value scaling (mini, mili, etc).

--- ioping.c
+++ ioping.c
@@ -58,6 +58,20 @@ timediff_t *do_ping() {
 }
 
 int main(int argc, char** argv) {
+	if(argc > 1) {
+		if(strcmp(argv[1], "config") == 0) {
+			printf("graph_title Naive IO latency\n"
+					"graph_args --base 1000 -l 0\n"
+					"graph_scale yes\n"
+					"graph_category disk\n"
+					"graph_info Runs a naive IO latency test with nanosecond accuracy (usually only as accurate as the platform allows), source code available at https://gitlab.com/Sharky/naive-ioping\n"
+					"graph_vlabel latency in seconds\n"
+					"sec.label seconds\n");
+			exit(0);
+		}
+		return 0;
+	}
+	
 	if( posix_memalign(&aligned_buffer, ___rand_bin_len, ___rand_bin_len) != 0) {
 		fprintf(stderr, "Unable to align buffer!\nerrno: %d %s\n", errno, strerror(errno));
 		exit(1);
@@ -66,20 +80,6 @@ int main(int argc, char** argv) {
 	if( memcpy(aligned_buffer, ___rand_bin, ___rand_bin_len) == NULL) {
 		fprintf(stderr, "Unable to memcpy to align buffer!\nerrno: %d %s\n", errno, strerror(errno));
 		exit(1);
-	}
-
-	if(argc > 1) {
-		if(strcmp(argv[1], "config") == 0) {
-			printf("graph_title Naive IO latency\n"
-					"graph_args --base 1000 -l 0\n"
-					"graph_scale yes\n"
-					"graph_category disk\n"
-					"graph_info Runs a naive IO latency test with nanosecond accuracy (usually only as accurate as the platform allows), source code available at https://github.com/vmp32k/naive-ioping\n"
-					"graph_vlabel latency in ms\n"
-					"ms.label milliseconds\n");
-			exit(0);
-		}
-		return 0;
 	}
 
 	double total = 0;
@@ -96,8 +96,8 @@ int main(int argc, char** argv) {
 		sleep(SLEEP_BETWEEN_RUNS);
 	}
 
-	total = (total / (double)NUM_IOPING_RUNS) * 1e-6;
-	printf("ms.value %lf\n", total);
+	total = (total / (double)NUM_IOPING_RUNS) * 1e-9;
+	printf("sec.value %lf\n", total);
 
 	for(int i = 0; i < NUM_IOPING_RUNS; ++i) {
 		free(runs[i]);
